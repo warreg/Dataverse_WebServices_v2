ac51bb479bf08f54a8d5edf12edac8844d45bf2e
Gerard WADE <38684089+warreg@users.noreply.github.com>
2022-08-03 15:37:11 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,13 +9,13 @@ TOKEN_URL = SERVER_URL + '/connect/token'
 WEBSITE_ID = "2" # "1"
 CLIENT_ID = 'Mirri'
 CLIENT_SECRET = '123456'
-USERNAME = 'gerard.wade@inrae.fr'
-PASSWORD = '827avat@R'
+USERNAME = 'user@mirri.org'
+PASSWORD = 'pwd'
 API_VERSION = 'v2'
 
 # DATAVERSE CLIENT
 DATAVERSE_SERVER = 'http://dataverse.mirri.org:8080'
-API_TOKEN = '326f6f49-7ff7-43cf-9e6a-3860e3cf7b0f'  #'2aad287c-63e1-485d-97f7-3d0d32cdff7b'
+API_TOKEN = 'gj743fer-ghh6y-7gft56h-8gfr5-vge67gf56'  
 PARENT = 'dvtest' # or 'root' or 'yeasts' etc.
 MAJOR_OR_MINOR = 'major'
 
